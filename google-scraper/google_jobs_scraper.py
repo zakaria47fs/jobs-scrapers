5ce02a6dede0477010fef3861cb1745d4b4df339
Anouar Bouchama <76883654+anouarbouchama@users.noreply.github.com>
2023-07-16 21:40:36 +0100

    Delete google_jobs_scraper.py

--- google-scraper/google_jobs_scraper.py
+++
@@ -1,6 +0,0 @@
-from utils import google_jobs_scrape
-from constants import SEARCH_QUERIES
-
-if __name__=='__main__':
-    for search_query in SEARCH_QUERIES:
-        jobs_data = google_jobs_scrape(search_query)
